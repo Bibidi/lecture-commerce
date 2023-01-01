2470995d3ebd2cea389721071a49a010a99625c7
Bibidi <byungdeuk.kim@gmail.com>
2023-01-02 08:10:29 +0900

    feat : [001-Email-Connect] mailgun에 대한 email 연동
    무료 계정으로 테스트 시 이메일 등록 반드시 필요.

---
+++ user-api/src/main/java/com/zerobase/cms/user/config/FeignConfig.java
@@ -0,0 +1,16 @@
+package com.zerobase.cms.user.config;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Qualifier(value = "mailgun")
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("api", "c2df74db42e194321cf38fdaee6121f2-c2efc90c-5a639b2c");
+    }
+}
